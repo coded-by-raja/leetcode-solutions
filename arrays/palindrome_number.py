915eb76289886f2c86684cf0e33161a2db12c194
Rajasakthieswaran G <grajasakthieswaran@gmail.com>
2026-01-12 21:23:21 +0530

    Day 1 – Palindrome Number

---
+++ arrays/palindrome_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return  False 
+        rev_half = 0
+        
+        while x > rev_half:
+            rev_half = rev_half * 10 + x % 10
+            x //= 10
+        return x== rev_half or x == rev_half // 10
+    
+
+        
