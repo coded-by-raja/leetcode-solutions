a2db6c4d011dbea7d8765db8d0df527a58f12124
Rajasakthieswaran G <grajasakthieswaran@gmail.com>
2026-01-19 22:59:44 +0530

    Day 9 – Length of Last Word

---
+++ strings/length_of_last_word.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        i = len(s) - 1
+        l = 0
+        while i >= 0 and s[i] == ' ':
+            i -=1
+        while i >= 0 and s[i] != ' ':
+            l +=1
+            i -=1
+        return l
